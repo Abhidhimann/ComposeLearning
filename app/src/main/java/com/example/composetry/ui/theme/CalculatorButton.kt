064566af7cee41898f1da177e25b2efdde7ece54
Abhishek <abhishekdhimaniitg@gmail.com>
2024-02-20 00:57:56 +0900

    simpleCalculator made

---
+++ app/src/main/java/com/example/composetry/ui/theme/CalculatorButton.kt
@@ -0,0 +1,27 @@
+package com.example.composetry.ui.theme
+
+enum class CalculatorButton(val value: String) {
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    DECIMAL("."),
+    ZERO("0"),
+    EQUALS("="),
+    PLUS("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    PERCENTAGE("%"),
+    DIVIDE("/"),
+    PARENTHESIS("( )"),
+    AC("AC"),
+    DELETE("DEL"),
+    START_PARENTHESIS("("),
+    END_PARENTHESIS(")"),
+    ERROR("ERROR")
+}
