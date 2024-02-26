cdc38996dee60937dab8c7e03b1c6c7db6fd9e9d
Abhishek <abhishekdhimaniitg@gmail.com>
2024-02-26 22:41:01 +0900

    fix: Simple chat list frontend

--- app/src/main/java/com/example/composetry/MainActivity.kt
+++ app/src/main/java/com/example/composetry/MainActivity.kt
@@ -1,273 +1,169 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.composetry
 
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composetry.ui.theme.CalculatorButton
+import coil.compose.rememberImagePainter
+import com.example.composetry.ui.theme.BackGround
 import com.example.composetry.ui.theme.ComposeTryTheme
-import javax.script.ScriptEngineManager
+import com.example.composetry.ui.theme.Green
+import com.example.composetry.ui.theme.Red
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTryTheme {
-                DefaultPreview()
+                MainScreen()
             }
         }
     }
-}
-
-interface CalculatorOperations {
-    fun clear()
-    fun write(calculatorButton: CalculatorButton)
-    fun delete()
-    fun calculate()
-}
-
-
-fun evaluateExpression(expression: String): Double? {
-    val scriptEngineManager = ScriptEngineManager()
-    val scriptEngine = scriptEngineManager.getEngineByName("js")
-
-    try {
-        return (scriptEngine.eval(expression) as? Number)?.toDouble()
-    } catch (e: Exception) {
-        // Handle potential errors during evaluation
-        e.printStackTrace()
-    }
-
-    return null
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    val calculatorButtonsList = listOf(
-        listOf(
-            CalculatorButton.AC,
-            CalculatorButton.PARENTHESIS,
-            CalculatorButton.PERCENTAGE,
-            CalculatorButton.DIVIDE
-        ),
-        listOf(
-            CalculatorButton.SEVEN,
-            CalculatorButton.EIGHT,
-            CalculatorButton.NINE,
-            CalculatorButton.MULTIPLY
-        ),
-        listOf(
-            CalculatorButton.FOUR,
-            CalculatorButton.FIVE,
-            CalculatorButton.SIX,
-            CalculatorButton.MINUS
-        ),
-        listOf(
-            CalculatorButton.ONE,
-            CalculatorButton.TWO,
-            CalculatorButton.THREE,
-            CalculatorButton.PLUS
-        ),
-        listOf(
-            CalculatorButton.ZERO,
-            CalculatorButton.DECIMAL,
-            CalculatorButton.DELETE,
-            CalculatorButton.EQUALS
-        )
+fun MainScreen(users: List<UserProfile> = usersProfile) {
+    Scaffold(topBar = { AppBar() }) {
+        mySurface(it, users)
+    }
+}
+
+@Composable
+fun AppBar() {
+    CenterAlignedTopAppBar(
+        navigationIcon =
+        { Icon(Icons.Default.Home, "Home", modifier = Modifier.padding(horizontal = 12.dp)) },
+        title = { Text("Simple Chat APP") },
     )
-    val text = remember { mutableStateOf("") }
-    val textStyleBody1 = TextStyle(fontSize = 60.sp)
-    var textStyle by remember { mutableStateOf(textStyleBody1) }
-    var readyToDraw by remember { mutableStateOf(false) }
-    val errorState = remember { mutableStateOf("") }
-    val calculatorOperations = object : CalculatorOperations {
-        override fun clear() {
-            text.value = ""
-            textStyle = textStyleBody1
-        }
+}
 
-        override fun write(calculatorButton: CalculatorButton) {
-            errorState.value = ""
-            if (calculatorButton.value == CalculatorButton.PARENTHESIS.value) {
-                if (text.value.endsWith(CalculatorButton.START_PARENTHESIS.value)
-                    || text.value.endsWith(CalculatorButton.PLUS.value)
-                    || text.value.endsWith(CalculatorButton.MINUS.value)
-                    || text.value.endsWith(CalculatorButton.MULTIPLY.value)
-                    || text.value.endsWith(CalculatorButton.DIVIDE.value)
-                ) {
-                    text.value += CalculatorButton.START_PARENTHESIS.value
-                } else {
-                    val endParenthesisCount =
-                        text.value.count { it.toString() == CalculatorButton.END_PARENTHESIS.value }
-                    val startParenthesisCount =
-                        text.value.count { it.toString() == CalculatorButton.START_PARENTHESIS.value }
-                    if (startParenthesisCount - endParenthesisCount == 0) {
-                        text.value += CalculatorButton.START_PARENTHESIS.value
-                    } else {
-                        if (endParenthesisCount < startParenthesisCount) {
-                            text.value += CalculatorButton.END_PARENTHESIS.value
-                        }
-                    }
-                }
-            } else {
-                text.value += calculatorButton.value
-            }
-        }
-
-        override fun delete() {
-            text.value = text.value.dropLast(1)
-            if (textStyle.fontSize<textStyleBody1.fontSize) {
-                textStyle = textStyle.copy(fontSize = textStyle.fontSize / 0.9)
-            }
-        }
-
-        override fun calculate() {
-            val result = evaluateExpression(text.value)
-            text.value = if (result != null) {
-                val formattedResult = if (result % 1 == 0.0) {
-                    result.toInt().toString()
-                } else {
-                    result.toString()
-                }
-                formattedResult
-            } else {
-                errorState.value = CalculatorButton.ERROR.value
-               text.value
-            }
-
-        }
-
-    }
+@Composable
+fun mySurface(appBar: PaddingValues, users: List<UserProfile>) {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color.LightGray
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = appBar.calculateTopPadding()),
+        color = BackGround
     ) {
-        Column(
+        LazyColumn(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Bottom,
+                .fillMaxWidth()
+                .wrapContentHeight(align = Alignment.Top),
+            verticalArrangement = Arrangement.SpaceAround
         ) {
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = 20.dp)
-                    .background(Color.White),
-            ) {
-                Column(modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.End) {
-                    Text(
-                        text = text.value,
-                        maxLines = 1,
-                        style = textStyle,
-                        softWrap = false,
-                        modifier = Modifier.drawWithContent {
-                            if (readyToDraw) drawContent()
-                        },
-                        onTextLayout = { textLayoutResult: TextLayoutResult ->
-                            if (textLayoutResult.didOverflowWidth) {
-                                textStyle = textStyle.copy(fontSize = textStyle.fontSize * 0.9)
-                            } else {
-                                readyToDraw = true
-                            }
-                        }
-                    )
-                    if(errorState.value==CalculatorButton.ERROR.value) {
-                        Text(
-                            text = CalculatorButton.ERROR.value,
-                            fontSize = 40.sp
-                        )
-                    }
-                }
-            }
-            Column(
-                verticalArrangement = Arrangement.Bottom
-            ) {
-                calculatorButtonsList.forEach {
-                    CalculatorRow(
-                        list = it,
-                        buttonSize = 80.dp,
-                        bottomPadding = 6.dp,
-                        horizontalSpacing = 12.dp,
-                        calculatorOperations = calculatorOperations
-                    )
-                }
+            items(users) {
+                ProfileCard(userProfile = it)
             }
         }
     }
-
 }
 
-
 @Composable
-fun CalculatorRow(
-    list: List<CalculatorButton>,
-    buttonSize: Dp,
-    bottomPadding: Dp,
-    horizontalSpacing: Dp,
-    calculatorOperations: CalculatorOperations
-) {
-    Row(
+fun ProfileCard(userProfile: UserProfile) {
+    Card(
+        shape = CutCornerShape(topEnd = 20.dp),
         modifier = Modifier
-            .wrapContentWidth()
-            .padding(bottom = bottomPadding),
-        horizontalArrangement = Arrangement.spacedBy(horizontalSpacing)
+            .padding(top = 12.dp, start = 12.dp, end = 12.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        elevation = CardDefaults.elevatedCardElevation(8.dp)
     ) {
-        list.forEach {
-            CircleButton(button = it, size = buttonSize) {
-                operation(it, calculatorOperations)
-            }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfilePicture(userProfile.picId, userProfile.status)
+            ProfileDetails(userProfile.name, userProfile.status)
         }
     }
 }
 
-fun operation(calculatorButton: CalculatorButton, calculatorOperations: CalculatorOperations) {
-    when (calculatorButton) {
-        CalculatorButton.AC -> calculatorOperations.clear()
-        CalculatorButton.DELETE -> calculatorOperations.delete()
-        CalculatorButton.EQUALS -> calculatorOperations.calculate()
-        else -> calculatorOperations.write(calculatorButton)
+// can also use this
+//Image(
+//painter = painterResource(id = R.drawable.sample_image),
+//contentDescription = "ProfilePicture",
+//modifier = Modifier
+//.padding(16.dp)
+//.size(72.dp)
+//.clip(CircleShape),
+//contentScale = ContentScale.Crop,
+//)
+@Composable
+fun ProfilePicture(picUrl: String, isOnline: Boolean) {
+    Card(
+        shape = CircleShape,
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        border = BorderStroke(
+            width = 2.dp,
+            color = if (isOnline) Green else Red
+        ),
+        modifier = Modifier.padding(12.dp)
+    ) {
+//        Image(
+//            painter = painterResource(id = picId)
+//            contentDescription = "ProfilePicture",
+//            modifier = Modifier
+//                .size(72.dp),
+//            contentScale = ContentScale.Crop,
+//        )
+        Image(
+            painter = rememberImagePainter(data = picUrl,
+            builder = {
+                placeholder(R.drawable.dog_pic) // default pic
+                error(Color.Green) // on error
+            }),
+            contentDescription = "ProfilePicture",
+            modifier = Modifier
+                .size(72.dp),
+            contentScale = ContentScale.Crop,
+        )
     }
 }
 
 @Composable
-fun CircleButton(button: CalculatorButton, size: Dp, onClick: () -> Unit) {
-    Box(
+fun ProfileDetails(userName: String, isOnline: Boolean) {
+    Column(
         modifier = Modifier
-            .size(size)
-            .clip(CircleShape)
-            .background(Color.DarkGray)
-            .clickable {
-                onClick()
-            },
-        contentAlignment = Alignment.Center
+            .padding(bottom = 8.dp, end = 8.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
     ) {
         Text(
-            text = button.value,
-            fontSize = 36.sp,
-            color = Color.White,
+            text = userName,
+            style = MaterialTheme.typography.titleMedium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Status: ${if (isOnline) "Active" else "Offline"}",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.alpha(0.5f)
         )
     }
 }
+
--- app/src/main/java/com/example/composetry/ui/theme/Color.kt
+++ app/src/main/java/com/example/composetry/ui/theme/Color.kt
@@ -1,6 +1,10 @@
 package com.example.composetry.ui.theme
 
+import android.provider.CalendarContract
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.composetry.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +13,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green = Color.Green
+
+val Red = Color.Red
+
+val LightBlue800 = Color(0xFF0277BD)
+
+
+val BackGround: Color
+    @Composable
+    get() = colorResource(id = R.color.back_ground)
