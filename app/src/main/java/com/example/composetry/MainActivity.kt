4e805c8968fa63b9149c6f0cf57828ffb6046653
Abhishek <abhishekdhimaniitg@gmail.com>
2024-07-02 22:55:17 +0900

    text view rounded corner

--- app/src/main/java/com/example/composetry/MainActivity.kt
+++ app/src/main/java/com/example/composetry/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -168,6 +169,7 @@ fun MainScreen() {
                 modifier = Modifier
                     .weight(1f)
                     .padding(bottom = 20.dp)
+                    .clip(RoundedCornerShape(16.dp))
                     .background(Color.White),
             ) {
                 Column(
